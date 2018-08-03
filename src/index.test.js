61ad42f78b0e84d20b68124f87864124ea1e6e00
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-08-03 01:14:19 -0300

    improve src/index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 const getMinOrderValue = require('./index');
 
+// BTC
 test("should format the value 0.00100000 in unit BTC to 0.001", () => {
     expect(getMinOrderValue(0.00100000, "BTC")).toBe(0.001);
 });
@@ -14,4 +15,38 @@ test("should format the value 0.10100000 in unit BTC to 0.101", () => {
 
 test("should format the value 0.11100000 in unit BTC to 0.111", () => {
     expect(getMinOrderValue(0.11100000, "BTC")).toBe(0.111);
+});
+
+// ETH
+test("should format the value 0.01100000 in unit ETH to 0.01", () => {
+    expect(getMinOrderValue(0.01100000, "ETH")).toBe(0.01);
+});
+
+test("should format the value 0.01500000 in unit ETH to 0.01", () => {
+    expect(getMinOrderValue(0.01500000, "ETH")).toBe(0.01);
+});
+
+test("should format the value 0.10100000 in unit ETH to 0.10", () => {
+    expect(getMinOrderValue(0.10100000, "ETH")).toBe(0.10);
+});
+
+test("should format the value 0.11100000 in unit ETH to 0.11", () => {
+    expect(getMinOrderValue(0.11100000, "ETH")).toBe(0.11);
+});
+
+// BNB
+test("should format the value 0.10000000 in unit BNB to 0.1", () => {
+    expect(getMinOrderValue(0.10000000, "BNB")).toBe(0.1);
+});
+
+test("should format the value 0.00150000 in unit BNB to 0.1", () => {
+    expect(getMinOrderValue(0.11500000, "BNB")).toBe(0.1);
+});
+
+test("should format the value 0.10100000 in unit BNB to 0.1", () => {
+    expect(getMinOrderValue(0.10100000, "BNB")).toBe(0.1);
+}); 
+
+test("should format the value 0.11100000 in unit BNB to 0.1", () => {
+    expect(getMinOrderValue(0.11100000, "BNB")).toBe(0.1);
 });
